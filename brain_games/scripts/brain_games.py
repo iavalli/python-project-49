3a5f3d9789f28faddb1b460c1876befd2b97b199
Irina Valieva <iavalieva.ds@gmail.com>
2024-01-21 19:52:55 +0300

    feat second project step

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+    print("poetry run python -m brain_games.scripts.brain_games")
+    print("Welcome to the Brain Games!")
+
+if __name__ == '__main__':
+    main()
+

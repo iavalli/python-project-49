b277dca5c5a2261e0a77a6ed2c078b7608338143
Irina Valieva <iavalieva.ds@gmail.com>
2024-01-30 20:04:52 +0300

    fix: brain-even scripts (4,9)

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,4 @@
 import random
-from brain_games.runner import run_game
-from brain_games.constants import EVEN_INSTRUCTION
 
 
 def is_number_even(number):
@@ -11,7 +9,3 @@ def get_num_and_correct_ans() -> tuple:
     number = random.randint(1, 100)
     correct_answer = 'yes' if is_number_even(number) else 'no'
     return number, correct_answer
-
-
-def run_even_game():
-    run_game(get_num_and_correct_ans, EVEN_INSTRUCTION)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-from brain_games.games.even import run_even_game
+from brain_games.runner import run_game
+from brain_games.constants import EVEN_INSTRUCTION
+from brain_games.games.even import get_num_and_correct_ans
 
 
 def main():
-    print('brain-even\n')
     run_even_game()
+
+
+def run_even_game():
+    run_game(get_num_and_correct_ans, EVEN_INSTRUCTION)
 
 
 if __name__ == '__main__':
